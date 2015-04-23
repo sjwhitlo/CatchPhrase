ae828c4c4c400fa5e9518c5d5a624a70af1167b1
sjwhitlo <sjwhitlo@ncsu.edu>
2015-04-23 15:08:40 -0400

    Created test files.

---
+++ CatchPhrase/test/theOnlyPackage/GameControllerTest.java
@@ -0,0 +1,69 @@
+package theOnlyPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGameControllerScanner() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetTeamOne() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetTeamTwo() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddTeamOne() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddTeamTwo() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSubTeamOne() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSubTeamTwo() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetPhrase() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSkipPhrase() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetCategory() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSelectCategory() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ CatchPhrase/test/theOnlyPackage/PhraseTest.java
@@ -0,0 +1,34 @@
+package theOnlyPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PhraseTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testPhrase() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetPhrase() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetCategory() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testIsCategory() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
